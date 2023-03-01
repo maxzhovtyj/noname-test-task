a23e05ff49114ec7c0049fec2b1387c7bb6b58fa
Zhovtaniuk Maxym <zhovtyjshady@gmail.com>
2023-03-01 23:58:24 +0200

    profile refactored

--- src/components/main/profile/profile.component.js
+++ src/components/main/profile/profile.component.js
@@ -1,18 +1,18 @@
 import {userSignOut} from "../../../services/AuthService";
 import {Button, Divider} from "@mui/material";
-import {getAppAuth} from "../../../context/AuthContext";
 import EmailIcon from '@mui/icons-material/Email';
 
 import classes from "./profile.module.css"
+import {useAuthContext} from "../../../context/AuthContext";
 
 function ProfileComponent() {
-    const auth = getAppAuth()
+    const {currentUser} = useAuthContext()
 
     return (
         <div>
             <h2>Profile</h2>
             <h4 className={classes.emailLabel}><EmailIcon/> Email:</h4>
-            <p className={classes.emailValue}>{auth.currentUser?.email}</p>
+            <p className={classes.emailValue}>{currentUser.email}</p>
             <Divider/>
             <Button onClick={userSignOut} className={classes.signOutBtn} variant={"outlined"}>Sign out</Button>
         </div>
