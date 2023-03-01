17d6a48b33e3d47412f1d33c9334ff09e24b1bd2
Zhovtaniuk Maxym <zhovtyjshady@gmail.com>
2023-03-01 18:31:12 +0200

    style changes in footer and header

--- src/components/footer/footer.component.js
+++ src/components/footer/footer.component.js
@@ -4,7 +4,13 @@ function FooterComponent() {
     return (
         <footer>
             <div className={classes.footerContainer}>
-                <h2>NONAME DIGITAL</h2>
+                <div>
+                    <h2>NONAME DIGITAL</h2>
+                    <p>E-commerce Test project</p>
+                </div>
+                <p className={classes.footerDescription}>
+                    Lorem ipsum dolor sit amet.<br/>Et totam minima aut corrupti repellat et rerum nulla.
+                </p>
             </div>
         </footer>
     );
