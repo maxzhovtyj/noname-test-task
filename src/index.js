993932ed4009e66191357b21caf5a059c115a73f
Zhovtaniuk Maxym <zhovtyjshady@gmail.com>
2023-03-01 19:11:25 +0200

    nginx added

--- src/index.js
+++ src/index.js
@@ -6,9 +6,6 @@ import reportWebVitals from './reportWebVitals';
 import {store} from "./redux/store"
 import {Provider} from "react-redux";
 
-// Importing the Bootstrap CSS
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Provider store={store}>
