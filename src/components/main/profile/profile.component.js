25eb8fbcccdcd1f285666dcb9f0d51db4645f532
Zhovtaniuk Maxym <zhovtyjshady@gmail.com>
2023-03-01 22:57:07 +0200

    profile component ui changed

--- src/components/main/profile/profile.component.js
+++ src/components/main/profile/profile.component.js
@@ -1,16 +1,20 @@
 import {userSignOut} from "../../../services/AuthService";
-import {Button} from "@mui/material";
+import {Button, Divider} from "@mui/material";
 import {getAppAuth} from "../../../context/AuthContext";
+import EmailIcon from '@mui/icons-material/Email';
+
+import classes from "./profile.module.css"
 
 function ProfileComponent() {
     const auth = getAppAuth()
 
     return (
         <div>
-            <h2>Profile component</h2>
-            <p>Email:</p>
-            <p>{auth.currentUser.email}</p>
-            <Button onClick={userSignOut} variant={"outlined"}>Sign out</Button>
+            <h2>Profile</h2>
+            <h4 className={classes.emailLabel}><EmailIcon/> Email:</h4>
+            <p className={classes.emailValue}>{auth.currentUser?.email}</p>
+            <Divider/>
+            <Button onClick={userSignOut} className={classes.signOutBtn} variant={"outlined"}>Sign out</Button>
         </div>
     );
 }
